e7e2846924b71c4f71cd6583bd8800ea9f508e8d
songliang <songliangliang@istarfinancial.cn>
2018-02-05 13:58:04 +0800

    change path

--- log-ip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-##统计日志中访问最多的的十个IP
-#awk '{print $1"  "$18}' /tmp/nginx_log/access.log |grep -v yunjiankong|sort|uniq -c|sort -n|tail
-awk '{print $1"  "$18}' /tmp/nginx_log/access.log |sort|uniq -c|sort -n|tail -15
--- webautoback_排除某个某个目录不备份.bat
+++
@@ -1,6 +0,0 @@
-@echo 复制WEB数据...请稍等
-"C:\Program Files\WinRAR\Rar" a -r -x*\wordpress -x*\wxych D:\autoback\wwwroot%date% D:\wwwroot
-@echo 复制WEB数据...请稍等
-"C:\Program Files\WinRAR\Rar" a -r D:\autoback\87895577_%date% D:\wwwroot\wxych
-@echo 复制WEB数据...请稍等
-"C:\Program Files\WinRAR\Rar" a -r D:\autoback\52237377_%date% D:\wwwroot\wordpress
