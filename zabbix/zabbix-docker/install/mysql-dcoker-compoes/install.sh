9c8dde107898eda7e88e99292e005a935c3ba245
liang <song-liang@live.com>
2018-05-01 19:59:19 +0800

    整理

--- zabbix/zabbix-docker/install/mysql-dcoker-compoes/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker exec -t zabbix_mysql mysql -uroot -pmysql -e "create database zabbix character set utf8;"
-docker exec -t zabbix_mysql mysql -uroot -pmysql -e "grant all on zabbix.* to 'zabbix'@'%' identified by 'zabbix';"
