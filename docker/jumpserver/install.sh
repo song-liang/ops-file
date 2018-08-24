b8ea30d11c4f8ac3435eab844d257bc26b6c6bfc
songliang <songliangliang@istarfinancial.cn>
2018-08-24 19:29:30 +0800

    修改 Dockerfile分层run

--- docker/jumpserver/install.sh
+++ docker/jumpserver/install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-## docker images构建镜像前的代码拉取初始化设置
+## WebSocket Server，jumpserver，luna代码下载，python环境镜像
 
-yum install -y vim wget git
+yum install -y vim wget git unzip
 ##  拉取相应代码
 ## WebSocket Server: Coco
-git clone https://github.com/jumpserver/coco.git && cd coco && git checkout master
-## jumpserver
-git clone https://github.com/jumpserver/jumpserver.git && cd jumpserver && git checkout master
+#git clone https://github.com/jumpserver/coco.git && cd coco && git checkout master
+wget https://codeload.github.com/jumpserver/coco/zip/master -O coco.zip
+unzip coco.zip && mv `unzip -l coco.zip | awk '{if(NR == 5){ print $4}}'` coco  
 
-wget https://github.com/jumpserver/luna/releases/download/1.4.0/luna.tar.gz
-tar xvf luna.tar.gz
-chown -R root:root luna
+## jumpserver
+#git clone https://github.com/jumpserver/jumpserver.git && cd jumpserver && git checkout master
+wget https://codeload.github.com/jumpserver/jumpserver/zip/master -O jumpserver.zip
+unzip jumpserver.zip && mv `unzip -l jumpserver.zip | awk '{if(NR == 5){ print $4}}'` jumpserver
+
+wget https://codeload.github.com/jumpserver/luna/zip/1.4.0 -O luna-1.4.0.zip
+unzip luna-1.4.0.zip && mv `unzip -l luna-1.4.0.zip | awk '{if(NR == 5){ print $4}}'` luna
+
+mkdir temp && mv *.zip temp
+
+## 构建python环境镜像
+docker build -t jumpserver-env .
