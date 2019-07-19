052ff9c09f059e767284308a5416056dce883f16
songliang <1107179995@qq.com>
2019-07-19 15:02:45 +0800

    修改 xtrabackup ，添加选项

--- other/xtrabackup.sh
+++ other/xtrabackup.sh
@@ -1,28 +1,83 @@
+#!/bin/bash
+
+# 2019.7.19 songliang
+
+# xtrabackup 备份mysql, 例子为备份单库，备份全库时请删除下方xtrabackup  --databases 参数
+
+HOST=192.168.1.120
+PORT=3307
+USER='root'
+PASSWRD='ROOT123'
+DATABASES='mall'			# 指定备份的库，
+DATA_DIR=/data/db/mysql/data		#  datadir 数据目录
+
+DIR_BACKUP=/data/db/mysql/backup						# 备份主目录
+DIR_DAILY_FULL_BAK=$DIR_BACKUP/$(date +%Y-%m-%d)/full				# 全量备份目录
+DIR_Hour_IN_BAK=$DIR_BACKUP/$(date +%Y-%m-%d)/$(date +%Y-%-m-%d-%H-%M)          # 增量备份目录
 
 
-Host=172.22.14.134
-User='root'
-Passwd=''
-Datebases='wl_mall_v2'
-Full_target_Dir='/data/mysql/backup/$DATE'
-DATE=$(date +%m%d-%H.%M)
+# 全量备份
+full_bak () {
 
-# 全量
-xtrabackup -H $Host -P 3306 --user=$User --password=$Passwd  \
-	--backup --parallel=2 --databases=$Datebases 
-	--target-dir=$Full_target_Dir
+		# 创建当日全量备份目录
+		mkdir -p $DIR_DAILY_FULL_BAK
 
-# 增量
-xtrabackup -H $Host -P 3306 --user=$User --password=$Passwd  \
-	--backup --parallel=2 --databases=$Datebases  \
-	--incremental-basedir=/data/mysql/backup/databak \
-	--target-dir=/data/mysql/backup/databak1 
+		# 执行备份
+		xtrabackup -H  $HOST -P $PORT --user=$USER --password=$PASSWRD \
+		--backup --parallel=2 \
+		--databases=$DATABASES \
+		--datadir=$DATA_DIR \
+		--target-dir=$DIR_DAILY_FULL_BAK
+}
 
+# 增量备份
+incremental_bak() {
+		# 创建增量备份目录
+		mkdir -p $DIR_Hour_IN_BAK
 
+		xtrabackup -H  $HOST -P $PORT --user=$USER --password=$PASSWRD \
+		--backup --parallel=2 \
+		--databases=$DATABASES \
+		--datadir=$DATA_DIR \
+		--incremental-basedir=$DIR_DAILY_FULL_BAK \
+		--target-dir=$DIR_Hour_IN_BAK
+}
 
-# 恢复
-xtrabackup --prepare --apply-log-only --target-dir=/data/mysql/backup/databak
-xtrabackup --prepare --apply-log-only --target-dir=/data/mysql/backup/databak --incremental-dir=/data/xtrabackup1/
-xtrabackup --prepare --apply-log-only --target-dir=/data/mysql/backup/databak --incremental-dir=/data/mysql/backup/databak2
+tar_clean() {
+		#压缩一天前的备份,删除目录文件，删除7天前的备份文件
+		cd $DIR_BACKUP
+		if [ -d $(date  -d '1 days ago' +%Y-%m-%d) ];then
+		    tar zcvf full_(date  -d '1 days ago' +%Y-%m-%d).tar.gz  $(date  -d '1 days ago' +%Y-%m-%d)  --remove-files
+		    rm -rf `find . -name '*.tar.gz' -mtime 7`
+		else
+		    echo "没有一天前的备份目录"
+		fi
+}
 
-xtrabackup --prepare --target-dir=/data/mysql/backup/databak
+case $1  in
+ 	full)
+		echo "开始全量备份"
+		full_bak
+		tar_clean
+		;;
+		
+	incremental)
+		if [ ! -d $DIR_DAILY_FULL_BAK ]; 
+		then
+		   echo "没有全量备份目录，执行全量备份"
+		   full_bak
+		   tar_clean
+		else
+		   echo "增量备份"
+		   incremental_bak
+		fi
+		;;
+
+	 *)
+		echo -e "\033[36mPlease select: \033[0m"
+		echo -e "\033[31m full : 全量备份 \033[0m"
+		echo -e "\033[31m incremental : 增量备份 \033[0m"
+		echo -e "\033[33m example:\n./xtrabackup.sh full \033[0m"
+		;;
+
+esac
